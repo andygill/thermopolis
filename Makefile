f593947ab569934388002d2f744e0c5eea51c755
Andy Gill <andygill@ku.edu>
2015-08-07 15:08:39 -0500

    Hello World of https

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+# Make a self-signing cert
+self::
+	openssl genrsa -des3 -out server.key 1024
+	openssl req -new -key server.key -out server.csr
+	cp server.key server.key.org
+	openssl rsa -in server.key.org -out server.key
+	openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,15 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-main = print ""
+import           Data.Monoid    (mconcat)
+import           Web.Scotty
+import           Web.Scotty.TLS
+
+tls :: ScottyM () -> IO ()
+-- tls = scottyTLS 3000 "server.key" "server.crt"
+tls = scotty 3000
+
+main :: IO ()
+main = tls $ do
+         get "/:word" $ do
+             beam <- param "word"
+             html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
