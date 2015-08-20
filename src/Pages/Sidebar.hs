7f7e70390c7d5bf4c7f9a842b519c5eedb6a2e27
Andy Gill <andygill@ku.edu>
2015-08-19 22:48:34 -0500

    Adding a sidebar

--- src/Pages/Sidebar.hs
+++ src/Pages/Sidebar.hs
@@ -3,6 +3,7 @@ module Pages.Sidebar where
 
 import           Data.Char        
 import           Data.Monoid
+import           Data.String
 import           Pages.Utils
 import           Data.Text(Text)
 import qualified Data.Text as T
@@ -12,22 +13,53 @@ data Sidebar
   = Classes [(String,Int)]
   
 sidebarPage :: ContentReader f => Sidebar -> f Page
-sidebarPage (Classes _) = readPage "sidebar.html" [
+sidebarPage (Classes clss) = readPage "sidebar.html" [
            ("content",mconcat <$> sequenceA
-                  [ sideLink "home" "Home!" 
-                  , sideLink "home" "Home!"
-                  ]
+                  ([ sideLink (glyphicon "home"
+                             <+> nbsp 
+                             <+> return "Home")
+                   ] ++ concat
+                   [ sideLink  (glyphicon "education" <+> nbsp <+> return (fromString cls))  :
+                     [ sideLink (nbsp  <+> nbsp  <+> nbsp  <+> nbsp <+> glyphicon "warning-sign"  
+                                                     <+> nbsp <+> return (fromString ("Homework " ++ show j)))
+                     | j <- [1..i]
+                     ]
+                   | (cls,i) <- clss
+                   ])
            )
    ]
 
+
+nbsp :: Applicative f => f Page
+nbsp = pure "&nbsp;"
+
+infixl 4 <+>
+
+(<+>) :: Applicative f => f Page -> f Page -> f Page
+(<+>) f g = mappend <$> f <*> g
+
+
+glyphicon :: ContentReader f => String -> f Page
+glyphicon name = readPage "glyphicon.html" [("glyphicon",return (fromString $ "glyphicon glyphicon-" ++ name))]
+
+-- "triangle-bottom"    
+
+
 -- TODO: add active
-sideLink :: ContentReader f => Page -> Page -> f Page
-sideLink glyphicon content = readPage "sidelink.html" 
-                   [ ("class",return "")
-                   , ("glyphicon",return glyphicon)
-                   , ("content",return content)
+sideLink :: ContentReader f => f Page -> f Page
+sideLink content = readPage "sidelink.html" 
+                   [ ("class",return "")        -- active if *this* page
+                   , ("url",return "#")
+                   , ("content",content)
                    ]
-                   
---nestBar :: ContentReader f => Text -> Page -> f Page -> f Page
---nestBar tag header = 
+
+
+{-                   
+nestBar :: ContentReader f => String -> Page -> f Page -> f Page
+nestBar tag header content = readPage "nestbar.html" 
+           [ ("target",return $ textToId tag)
+           , ("header",return header)
+           , ("content",content)
+           ]
+-}
 
